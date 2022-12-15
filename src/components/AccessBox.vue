8230db825ca90d1d85a4751902829879c88a4dd7
theSwankyNinja <pixiedaemon@yahoo.com>
2022-12-15 16:25:22 -0600

    wonky version, but I want to preserve it

---
+++ src/components/AccessBox.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { useAccessOptionsStore } from "@/stores/accessOptionsStore"
+  import { storeToRefs } from 'pinia';
+  const store = useAccessOptionsStore();
+  const { accessOptionsList } = storeToRefs(store)
+  const { toggleAccessibilityOption, fetchOptions } = store;
+  fetchOptions();
+
+</script>
+<template>
+  <div class="colorBeta" :class="{ monoBeta: hiContrastMode }">
+    <button @click.stop="toggleAccessibilityOption(showAccessOptions)">
+      Show Accessibility Options
+    </button>
+  </div>
+  <div v-if="showAccessOptions" id="accessibility" class="colorAlpha" :class="{ monoAlpha: hcmOn }">
+    <label for="hicontrast">Enable Hi-Contrast Mode</label>
+    <input type="checkbox" id="hicontrast" class="checkbox" @click.stop="toggleAccessibilityOption(hiContrastMode)"/>
+    <p class="colorAlpha">{{ hcmOn }}</p>
+  </div>
+</template>
+<style>
+#accessibility {
+  display: flex;
+  justify-content: flex-start;
+}
+</style>
