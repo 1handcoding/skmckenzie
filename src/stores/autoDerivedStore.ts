eca3100a6236ae87b200cc9de1fd0ddd08712c70
S. Kyle McKenzie <pixiedaemon@yahoo.com>
2023-01-16 15:51:01 -0600

    added preferrdMotion

--- src/stores/autoDerivedStore.ts
+++ src/stores/autoDerivedStore.ts
@@ -1,14 +1,16 @@
 import { defineStore } from 'pinia'
-import { useDevicePixelRatio, usePreferredColorScheme, useScreenOrientation, usePreferredContrast} from '@vueuse/core';
+import { useDevicePixelRatio, usePreferredColorScheme, useScreenOrientation, usePreferredContrast, usePreferredReducedMotion} from '@vueuse/core';
 
 export const useAutoDerivedStore = defineStore('autoDerivedStore', () => {
   const { pixelRatio } = useDevicePixelRatio()
-  const { preferredColor } = usePreferredColorScheme()
+  const preferredColor = usePreferredColorScheme()
   const {
     isSupported,
     orientation,
     angle,
   } = useScreenOrientation()
-const preferredContrast = usePreferredContrast()
-    return { isSupported, orientation, angle, pixelRatio, preferredColor, preferredContrast }
+  const preferredContrast = usePreferredContrast()
+  const preferredMotion = usePreferredReducedMotion()
+  
+    return { isSupported, orientation, angle, pixelRatio, preferredColor, preferredContrast, preferredMotion }
 })
