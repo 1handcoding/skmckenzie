e21031a1ef39200db3fcb2769cbf12a1d0641039
S. Kyle McKenzie <pixiedaemon@yahoo.com>
2023-02-01 17:41:18 -0600

    implemented visual toggles, added audio alert

--- src/components/AccessBar.vue
+++ src/components/AccessBar.vue
@@ -1,13 +1,29 @@
+<!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <script setup lang="ts">
 import { useAccessOptionsStore } from '@/stores/accessOptionsStore';
 import { ref } from 'vue';
 import { storeToRefs } from 'pinia';
+import { useSpeechSynthesis } from '@vueuse/core'
+
+
 const store = useAccessOptionsStore();
 const { hiContrastOn, bigButtonsOn } = storeToRefs(store);
 const { toggleHiContrastOn, toggleBigButtonsOn } = store;
+
+
 const bypassOpen = ref(false)
 const showAccess = ref(false)
-const hcmToggle = ref(false)
+let hcmToggle = ref(false)
+const bigButtonToggle = ref(false)
+
+let togIconA = "toggle_off"
+let togIconB = "toggle_off"
+
+const speech1 = new SpeechSynthesisUtterance('Hi-contrast is enabled')
+const speech2 = new SpeechSynthesisUtterance('Hi-contrast is disabled')
+const speech3 = new SpeechSynthesisUtterance('Button size set to default')
+const speech4 = new SpeechSynthesisUtterance('Button size set to large')
+
 
 function toggleBypass() {
   bypassOpen.value = !bypassOpen.value
@@ -15,11 +31,33 @@ function toggleBypass() {
 function accessOptions() {
   showAccess.value = !showAccess.value;
 }
-
+function speakHCM() {
+  if (hcmToggle.value) { window.speechSynthesis.speak(speech1) }
+  else if (!hcmToggle.value) { window.speechSynthesis.speak(speech2) }
+}
+function toggleHCM() { 
+  hcmToggle.value = !hcmToggle.value;
+  if (hcmToggle.value) {
+    togIconA="toggle_on"
+  }
+  else {
+    togIconA="toggle_off"
+  }
+}
+function toggleBigButtons() { 
+  bigButtonToggle.value = !bigButtonToggle.value;
+  if (bigButtonToggle.value) {
+    togIconB="toggle_on"
+  }
+  else {
+    togIconB="toggle_off"
+  }
+}
 </script>
 
 <template>
   <div>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <ul id="menubar" class="colorNegative" :class="{hcmNegative: hiContrastOn}">
         <ul><button type="button" :aria-expanded="bypassOpen" aria-controls="id_bypass_links" @click.stop="toggleBypass()">Bypass Links</button>
             <ul id="id_bypass_links" class="colorPositive" :class="{hcmPositive: hiContrastOn}" v-if="bypassOpen">
@@ -40,10 +78,10 @@ function accessOptions() {
         <ul><button type="button" :aria-expanded="showAccess" aria-controls="show_accessibility_options" @click.stop="accessOptions">Accessibility Options</button>
                 <ul id="id_about_menu" v-if="showAccess">
                     <li>
-                        <button @click.stop="toggleHiContrastOn()" :aria-pressed="`${hcmToggle}`">Toggle Hi-Contrast</button><div id="toggle" class="toggleoff" :class="{toggleon: hiContrastOn}"></div>
+                        <button @click.stop="toggleHiContrastOn(); toggleHCM(); speakHCM()" :aria-pressed="`${hcmToggle}`">Toggle Hi-Contrast</button><span class="material-symbols-outlined" :class="{toggleOn: hcmToggle}">{{togIconA}}</span>
                     </li>
                     <li>
-                        <button @click.stop="toggleBigButtonsOn()" :aria-pressed="`${bigButtonsOn}`">Toggle Big Buttons</button><div id="toggle" class="toggleoff" :class="{toggleon: bigButtonsOn}"></div>
+                        <button @click.stop="toggleBigButtonsOn(); toggleBigButtons()" :aria-pressed="`${bigButtonToggle}`">Toggle Big Buttons</button><span class="material-symbols-outlined" :class="{toggleOn: bigButtonToggle}">{{togIconB}}</span>
                     </li>
                 </ul>
               </ul>
@@ -74,7 +112,16 @@ width: 1.5em}
 .toggleoff {
   background-color: gray;
 }
-.toggleon {
-  background-color: #660330;
+
+.material-symbols-outlined {
+  font-variation-settings:
+  'FILL' 0,
+  'wght' 400,
+  'GRAD' 0,
+  'opsz' 48
+}
+.toggleOn {
+  font-variation-settings:
+  'FILL' 1;
 }
 </style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,11 +1,13 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 import { useAccessOptionsStore } from "@/stores/accessOptionsStore";
 import { storeToRefs } from 'pinia';
+import { useTitle, set, useMouseInElement, useCurrentElement } from '@vueuse/core';
 import ariaButton from '@/components/ariaButton.vue'
 const store = useAccessOptionsStore();
 const { hiContrastOn } = storeToRefs(store);
+const targetSection = ref(null)
 
 let expIcon = "expand_more";
 let expIcon2 = "expand_more";
@@ -15,8 +17,6 @@ let showHome = ref(false);
 let showAbout = ref(false);
 let showMethods = ref(false);
 let showNext = ref(false);
-
-const url= "url"
 
 function toggleHome() { 
   showHome.value = !showHome.value;
@@ -40,24 +40,24 @@ function toggleAbout() {
 function toggleMethods() { 
   showMethods.value = !showMethods.value;
   if (showMethods.value) {
-    expIcon2="expand_less"
+    expIcon3="expand_less"
   }
   else {
-    expIcon2="expand_more"
+    expIcon3="expand_more"
   }
 }
 function toggleNext() { 
   showNext.value = !showNext.value;
   if (showNext.value) {
-    expIcon2="expand_less"
+    expIcon4="expand_less"
   }
   else {
-    expIcon2="expand_more"
+    expIcon4="expand_more"
   }
 }
 function toggleExpand(nameExp: string, showProp: boolean) {
   if (nameExp) {
-    expIcon2="expand_less"
+    expIcon="expand_less"
   }
   else {
     expIcon2="expand_more"
@@ -67,23 +67,23 @@ function toggleExpand(nameExp: string, showProp: boolean) {
 <template>
   
   <div class="expandyBox">
-    <div class="secBar colorNegative" :class="{ hcmNegative: hiContrastOn }">
+    <div role="button" id="show_home" class="secBar colorNegative" :class="{ hcmNegative: hiContrastOn }" aria-controls="show_home" @click.prevent="toggleHome()" :aria-expanded="`${showHome}`" >
       <h2 id="homeSection" ref="homeSection">Home</h2>
-      <ariaButton  idProp="homeButton" @click.stop="toggleHome()"/><span class="material-symbols-rounded">{{ expIcon }}</span>
+      <ariaButton></ariaButton>
     </div>
     <div v-show="showHome" class="secAnchor colorPositive" :class="{ hcmPositive: hiContrastOn }">
       <p>Welcome to my accessibility demonstration</p>
     </div>
   </div>
   <div class="expandyBox">
-  <div role="button" id="show_about" class="expandyBox" :class="{ hcmNegative: hiContrastOn }" aria-controls="show_about" @click.prevent="toggleAbout()" :aria-expanded="`${showAbout}`" >
+  <div role="button" id="show_about" class="secBar colorNegative" :class="{ hcmNegative: hiContrastOn }" aria-controls="show_about" @click.prevent="toggleAbout()" :aria-expanded="`${showAbout}`" >
     <ariaButton boop='About Button'></ariaButton>
     </div>
     <div v-show="showAbout" class="dropDown">
-      <p>Welcome to my accessibility demonstration</p>
+      <p>Hi, I'm Kyle McKenzie, and I want to be an accessibility engineer. On this page, I'm experimenting with implementing better accessibility in Vue.js. As the usage of reactive frameworks is rapidly increasing, it is vital that accessibility practices keep pace. Reactive frameworks currently have an appallingly poor reputation for ease of access that must be addressed.</p>
     </div>
   </div>
-  <div class="expandyBox">
+  <div ref="methods" class="expandyBox">
     <div>
       <button type="button" :aria-expanded="`${showMethods}`" aria-controls="show_methods" class="secButton" @click.stop="toggleMethods()">
         Methods
@@ -98,15 +98,15 @@ function toggleExpand(nameExp: string, showProp: boolean) {
       <p>Conditional rendering can be used to toggle visibility. Here it is employed to initially hide advanced accessibility features. This allows accessibility options to be anchored at the top of the page, where they are easy to locate, without obstructing the page flow for users who don't want or need them</p>
 
       <h3 aria-level="4">Dynamic Classes</h3>
-      <p>Vue's ability to seamlessly alter CSS classes is leveraged to alter the rendering of individual page elements. 
-      While many users benefit from having larger buttons that are easier to see and press, 
-      a user with a limited field of vision or who uses screen enlargement might find the larger buttons disruptive. 
+      <p>Vue's ability to seamlessly alter CSS classes is leveraged to alter the rendering of individual page elements. This is helpful as some accessibility needs conflict, for example:
+      while many users benefit from having larger buttons that are easier to see and press, a user with a limited field of vision or a user who uses screen enlargement might find the larger buttons disruptive. 
       Being able to alternate between one or more options for an element helps the site meet a variety of needs simultaneously with a minimum of code.</p>
 
       <h3 aria-level="4">useTitle from vueUse</h3>
-      <p>While vueUse provides a tremendous number of useful functions, the site currently uses only useTitle. The title is dynamically updated with the name of the current tab panel.</p>
+      <p> The title is dynamically updated with the name of the current section</p>
 
-
+      <h3 aria-level="4">window.SpeechSynthesis</h3>
+      <p>Custom audio alerts are currently being added to toggle functions that are not automatically announced via screen reader. Implementation could potentially switch to useSpeechSynthesis from vueUse, but it was more difficult to implement initially.</p>
 
       <h3 aria-level="3">Auto-Derived Values from Browser</h3>
       <p>Various values and preferences are accessed using a variety of vueuse components. These can then be accessed from their own Pinia store and used to inform custom styling preferences throughout the site</p>
@@ -116,7 +116,7 @@ function toggleExpand(nameExp: string, showProp: boolean) {
         While almost universally helpful to users, this is especially beneficial to some disabled users.
         To maximize the benefit, this can, in theory, then be configured to work in conjunction with other settings</li>
 
-      <h3 aria-level="3">No Longer Used</h3>
+      <h3 aria-level="3" class="subheader">No Longer Used</h3>
         <h3 aria-level="4">Vue Router</h3>
           <p>The page formerly used Vue Router to approximate the ARIA <a href="https://www.w3.org/WAI/ARIA/apg/example-index/tabs/tabs-manual.html">tablist format</a>. 
         While this allowed the page to present in a familiar format, this could be better accomplished using a combination of dynamic classes and conditional rendering. 
@@ -124,7 +124,7 @@ function toggleExpand(nameExp: string, showProp: boolean) {
           </p>
     </div>
   </div>
-  <div class="expandyBox">
+  <div ref="next_steps" class="expandyBox">
     <div>
       <button type="button" :aria-expanded="`${showNext}`" aria-controls="show_next_steps" class="secButton" @click.stop="toggleNext()">
         Next Steps
@@ -134,7 +134,7 @@ function toggleExpand(nameExp: string, showProp: boolean) {
     <div v-show="showNext">
       <h2 id="nextSection" class="colorNegative" :class="{hcmNegative: hiContrastOn}">Next Steps</h2>
       <ul>
-        <li>Correct useTitle for use with "accordion-style" page</li>
+        <li>Correct useTitle for use with "accordion-style" page, using vueUse functionality to report which element is currently targeted by the user.</li>
         <li>Expand custom color styling to auto-load based on color preferences set in browser</li>
         <li>Test modifying custom button classes automatically in response to zoom level, device orientation</li>
         <li>Use vueUse to add accessible alerts/audio feedback</li>
@@ -142,7 +142,7 @@ function toggleExpand(nameExp: string, showProp: boolean) {
         <li>Create usable functions to manage focus</li>
         <li>Rectify all the ARIA labels, preferably by using dynamic HTML attributes</li>
         <li>Add CSS styling for all the new content</li>
-        <li>Reduce the amount of essentially redundant code w/ more dynamic templating</li>
+        <li>Reduce the amount of essentially redundant code even more w/ dynamic templating</li>
       </ul>
     </div>
   </div>
@@ -166,5 +166,8 @@ function toggleExpand(nameExp: string, showProp: boolean) {
   font-weight: inherit;
   line-height: inherit;
   padding: 0;
+}
+.subheader {
+  text-decoration: underline;
 }
 </style>
